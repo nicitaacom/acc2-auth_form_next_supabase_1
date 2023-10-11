583268bae9539eb0af8c48171ab050f481811bd7
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-10-11 07:25:12 +0000

    fix by supabase email support

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function middleware(req:NextRequest) {
-	const res = NextResponse.next();
-	const supabase = createMiddlewareClient({ req, res });
-	await supabase.auth.getSession();
-	return res;
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  await supabase.auth.getSession();
+  return res;
 }
