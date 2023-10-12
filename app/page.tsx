d0102fc35fa1e84f09f1b764facaa228d32eb772
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-10-12 17:03:54 +0000

    work to !work

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar/Navbar"
 import { AuthModal } from "./components/ui/Modals/AuthModal"
 import { AuthModalContainer } from "./components/ui/Modals/ModalContainers/AuthModalContainer"
 
-
 export default function Home() {
   return (
     <>
