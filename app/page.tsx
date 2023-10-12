b6777427c4358343d9b62ece83272066b500e6e7
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-10-12 16:44:43 +0000

    2 the same projects - 1 throw error - 2 no

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { AuthModal } from "./components/ui/Modals/AuthModal"
 import { AuthModalContainer } from "./components/ui/Modals/ModalContainers/AuthModalContainer"
 
 
-export const dynamic = 'force-dynamic'
-
-
 export default function Home() {
   return (
     <>
