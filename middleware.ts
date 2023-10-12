39b31140fce2011f6bb9473d3bd0ad715c5cbd8b
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-10-12 13:18:48 +0000

    temp fix

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-import type { Database } from "@/interfaces/types_db"
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
-  await supabase.auth.getSession()
+  
+  
   return res
 }
