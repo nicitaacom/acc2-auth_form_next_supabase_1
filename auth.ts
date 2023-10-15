8106ed6a034de9f45fc5e360cba32c1b16d3b20f
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-15 11:37:18 +0200

    still doesn't work

--- auth.ts
+++ auth.ts
@@ -4,10 +4,11 @@ import FaceitProvider from "next-auth/providers/faceit"
 export const authOptions: NextAuthOptions = {
   providers: [
     FaceitProvider({
-      clientId: process.env.NEXT_PUBLIC_FACEIT_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_FACEIT_CLIENT_SECRET!,
+      clientId: process.env.NEXT_PUBLIC_FACEIT_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEIT_CLIENT_SECRET,
     }),
   ],
 }
+
 export const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
