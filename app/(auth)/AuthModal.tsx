7f232c71fc9c3e314180e014f94211e8b0953170
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-26 09:51:02 +0100

    step3-inputValidation

--- app/(auth)/AuthModal.tsx
+++ app/(auth)/AuthModal.tsx
@@ -1,12 +1,70 @@
+import { useForm } from "react-hook-form"
+import { AiOutlineLock, AiOutlineMail, AiOutlineUser } from "react-icons/ai"
+
+import { AuthInput } from "@/components/ui/Inputs/Validation/AuthInput"
 import { ModalContainer } from "@/components/ui/Modals/ModalContainer"
 import { useAuthModal } from "@/store/ui/authModal"
+import { useState } from "react"
+
+interface FormData {
+  email: string
+  password: string
+  username: string
+}
+
+type Variant = "login" | "register" | "recover" | "resetPassword" | "recoverCompleted" | "authCompleted"
 
 export function AuthModal() {
+  const [isEmailSent, setIsEmailSent] = useState(false)
+  const [variant, setVariant] = useState<Variant>("register")
+
   const nameModal = useAuthModal()
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setFocus,
+    getValues,
+    reset,
+  } = useForm<FormData>()
 
   return (
     <ModalContainer className="relative w-full max-w-[450px]" isOpen={nameModal.isOpen} onClose={nameModal.closeModal}>
-      <div>some content here</div>
+      <div>
+        <AuthInput
+          endIcon={<AiOutlineMail size={24} />}
+          register={register}
+          errors={errors}
+          disabled={isSubmitting || isEmailSent}
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="user@big.com"
+          required
+        />
+        <AuthInput
+          endIcon={<AiOutlineLock size={24} />}
+          register={register}
+          errors={errors}
+          id="password"
+          label="Password"
+          type="password"
+          placeholder={variant === "register" || variant === "resetPassword" ? "NeW-RaNd0m_PasWorD" : "RaNd0m_PasWorD"}
+          disabled={isSubmitting || isEmailSent}
+          required
+        />
+        <AuthInput
+          endIcon={<AiOutlineUser size={24} />}
+          register={register}
+          errors={errors}
+          id="username"
+          label="Username"
+          placeholder="HANTARESpeek"
+          disabled={isSubmitting || isEmailSent}
+          required
+        />
+      </div>
     </ModalContainer>
   )
 }
---
+++ app/components/ui/Inputs/Validation/AuthInput.tsx
@@ -0,0 +1,110 @@
+import { FieldErrors, UseFormRegister } from "react-hook-form"
+import { twMerge } from "tailwind-merge"
+
+interface FormData {
+  email: string
+  password: string
+  username: string
+}
+
+interface AuthInputProps {
+  id: keyof FormData
+  type?: string
+  className?: string
+  label: string
+  placeholder?: string
+  startIcon?: React.ReactNode
+  endIcon?: React.ReactNode
+  register: UseFormRegister<FormData>
+  errors: FieldErrors
+  disabled?: boolean
+  required?: boolean
+}
+
+interface ValidationRules {
+  [key: string]: {
+    required: string
+    pattern: {
+      value: RegExp
+      message: string
+    }
+  }
+}
+
+export function AuthInput({
+  id,
+  type,
+  className,
+  label,
+  placeholder,
+  startIcon,
+  endIcon,
+  register,
+  errors,
+  disabled,
+  required,
+}: AuthInputProps) {
+  const validationRules: ValidationRules = {
+    username: {
+      required: "This field is required",
+      pattern: {
+        value: /^(?=.*[a-z])[a-z][a-z0-9]{2,15}$/i,
+        message: "3-16 characters, a-z, start with letter, numbers optional",
+      },
+    },
+    email: {
+      required: "This field is required",
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "Enter valid email address",
+      },
+    },
+    password: {
+      required: "This field is required",
+      pattern: {
+        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_\-%#$]{8,64}$/,
+        message: "Must contain a-z A-Z 0-9 and _-%#$ are optional - between 8-64 symbols",
+      },
+    },
+  }
+
+  const {
+    required: requiredMessage,
+    pattern: { value: patternValue, message: patternMessage },
+  } = validationRules[id]
+
+  return (
+    <div className="relative">
+      <label htmlFor={id}>
+        {label}
+        <div className="flex flex-col">
+          <div className="absolute left-2 translate-y-[35%] text-icon-color">{startIcon}</div>
+          <input
+            className={twMerge(
+              `
+          px-4 py-2 bg-background border border-border-color rounded text-subTitle placeholder:opacity-25 outline-none`,
+              startIcon && "pl-9",
+              endIcon && "pr-9",
+              disabled && "opacity-50 cursor-default pointer-events-none",
+              errors[id] && "focus:ring-rose-500 focus-visible:ring-rose-600"
+            )}
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            {...register(id, {
+              required: requiredMessage,
+              pattern: {
+                value: patternValue,
+                message: patternMessage,
+              },
+            })}
+          />
+          <div className="absolute right-2 translate-y-[35%] text-icon-color">{endIcon}</div>
+          {errors[id] && errors[id]?.message && (
+            <span className="text-danger">{errors[id]?.message as React.ReactNode}</span>
+          )}
+        </div>
+      </label>
+    </div>
+  )
+}
