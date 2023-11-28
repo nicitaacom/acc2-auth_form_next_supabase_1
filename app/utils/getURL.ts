5021441ac4fafba4123618fbb4a1bcc7b9b1c35f
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-28 17:14:13 +0100

    fix: getURL() to use devUrl in dev and prodUrl in prod
    Also if fix some strage url that vercel add to production link
    like:
    acc2-auth-form-next-supabase-1-b6jpvkkx0-nicitaacom.vercel.app

--- app/utils/getURL.ts
+++ app/utils/getURL.ts
@@ -1,11 +1,13 @@
 export const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/"
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL // Automatically set by Vercel.
+
   // Make sure to include `https://` when not localhost.
-  url = url.includes("http") ? url : `https://${url}`
+  url = url!.includes("http") ? url : `https://${url}`
   // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+  url = url!.charAt(url!.length - 1) === "/" ? url : `${url}/`
   return url
 }
