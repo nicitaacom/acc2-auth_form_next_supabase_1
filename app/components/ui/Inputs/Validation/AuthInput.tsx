f920b64b51b0ae38338cbaeeabb294b4eb788e60
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-12-04 16:58:20 +0100

    chore: new line deleted

--- app/components/ui/Inputs/Validation/AuthInput.tsx
+++ app/components/ui/Inputs/Validation/AuthInput.tsx
@@ -81,8 +81,7 @@ export function AuthInput({
           <div className="absolute left-2 translate-y-[35%] text-icon-color">{startIcon}</div>
           <input
             className={twMerge(
-              `
-          px-4 py-2 bg-background border border-border-color rounded text-subTitle placeholder:opacity-25 outline-none`,
+              `px-4 py-2 bg-background border border-border-color rounded text-subTitle placeholder:opacity-25 outline-none`,
               startIcon && "pl-9",
               endIcon && "pr-9",
               disabled && "opacity-50 cursor-default pointer-events-none",
